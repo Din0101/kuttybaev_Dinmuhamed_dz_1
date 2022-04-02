c290b19c9af8133273461d9eb370e1c26f3de47f
Din <dinmuhamed50@gmail.com>
2022-04-02 20:17:52 +0600

    add

--- 1_2_b.py
+++ 1_2_b.py
@@ -5,6 +5,10 @@ i = 1
 while i < 1000:
     user_lst.append(i * i * i)
     i += 2
+i=0
+while i < 500:
+    user_lst[i] += 17
+    i += 1
 print("Список кубов нечетных чисел: " , user_lst)
 for element in user_lst:
     user_lst2.append(str(element))
@@ -16,7 +20,4 @@ for element in user_lst2:
         i += 1
     if sum_elem % 7 == 0:
         print("число, сумма цифр кратная 7 :" , element)
-i=0
-for i in user_lst
-    user_lst[i] += 17
-    i += 1
+
